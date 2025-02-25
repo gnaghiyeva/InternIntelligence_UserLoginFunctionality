4e3612d815dee9982c338f810c7038b5a2f46cf7
gnaghiyeva <gulnar.nagiyeva17@gmail.com>
2025-02-25 22:37:57 +0400

    create structure

---
+++ src/test/java/org/example/login_register/LoginRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.login_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
